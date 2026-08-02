eac4bc53dbe316f73cd323a954c2ecb683ccf934
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-08-02 14:24:22 +0200

    TESTS: Assert that the RNG misuse warning message report on the iteration index

--- inst/testme/test-rng-misuse.R
+++ inst/testme/test-rng-misuse.R
@@ -20,13 +20,40 @@ res <- withCallingHandlers({
 # Test with multiple iterations in a chunk to trigger seq_to_human
 # We can force 1 chunk for all iterations
 res <- withCallingHandlers({
-  foreach(i = 1:5, .options.future = list(seed = FALSE, chunk.size = 10)) %dofuture% {
+  foreach(i = 1:5, .options.future = list(seed = FALSE, chunk.size = 10L)) %dofuture% {
     runif(1)
   }
 }, warning = function(w) {
   message("Caught expected warning: ", w$message)
   invokeRestart("muffleWarning")
 })
+
+
+# One iteration per chunk gives an "Iteration <idx>" message
+msgs <- character(0L)
+res <- withCallingHandlers({
+  foreach(i = 1:2, .options.future = list(seed = FALSE, chunk.size = 1L)) %dofuture% {
+    runif(1)
+  }
+}, warning = function(w) {
+  msgs <<- c(msgs, conditionMessage(w))
+  invokeRestart("muffleWarning")
+})
+stopifnot(length(msgs) == 2L, all(grepl("Iteration [0-9]+ of the foreach", msgs)))
+
+# Elements processed in a custom order are remapped in the message
+msgs <- character(0L)
+scheduling <- structure(1.0, ordering = rev(seq_len(4L)))
+res <- withCallingHandlers({
+  foreach(i = 1:4, .options.future = list(seed = FALSE, scheduling = scheduling)) %dofuture% {
+    runif(1)
+  }
+}, warning = function(w) {
+  msgs <<- c(msgs, conditionMessage(w))
+  invokeRestart("muffleWarning")
+})
+stopifnot(length(msgs) >= 1L, all(grepl("of the foreach", msgs)))
+message("Caught expected warning: ", msgs[1])
 
 
 # Test doFuture.rng.onMisuse = "ignore"
